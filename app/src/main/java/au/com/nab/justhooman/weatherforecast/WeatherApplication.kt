65a939e5d8da19ae58ee7145bc374fd2f68f7ce4
justHooman <minhtdh89@gmail.com>
2022-09-26 00:47:51 +0700

    gradle: add hilt, retrofit, lifecycle dependencies

---
+++ app/src/main/java/au/com/nab/justhooman/weatherforecast/WeatherApplication.kt
@@ -0,0 +1,7 @@
+package au.com.nab.justhooman.weatherforecast
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApplication : Application()
